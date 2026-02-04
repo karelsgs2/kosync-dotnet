54e2ef1fd4241ed189af30e33d30e71dd237967c
karelsgs2 <65086321+karelsgs2@users.noreply.github.com>
2026-02-04 21:00:57 +0100

    Update Document.cs

--- Database/Entities/Document.cs
+++ Database/Entities/Document.cs
@@ -3,15 +3,15 @@ namespace Kosync.Database.Entities;
 
 public class Document
 {
-    public string DocumentHash { get; set; } = default!;
+    public string DocumentHash { get; set; } = string.Empty;
 
-    public string Progress { get; set; } = default!;
+    public string Progress { get; set; } = string.Empty;
 
-    public decimal Percentage { get; set; } = default!;
+    public double Percentage { get; set; }
 
-    public string Device { get; set; } = default!;
+    public string Device { get; set; } = string.Empty;
 
-    public string DeviceId { get; set; } = default!;
+    public string DeviceId { get; set; } = string.Empty;
 
-    public DateTime Timestamp {get;set;} = default!;
+    public DateTime Timestamp { get; set; }
 }
