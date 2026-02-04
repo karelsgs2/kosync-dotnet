57469aa1d8a93a6c6c94bd8218a5e71bf5fa06c0
karelsgs2 <65086321+karelsgs2@users.noreply.github.com>
2026-02-04 21:01:50 +0100

    Update DocumentRequest.cs

--- Models/DocumentRequest.cs
+++ Models/DocumentRequest.cs
@@ -7,7 +7,7 @@ public class DocumentRequest
 
     public string progress { get; set; } = default!;
 
-    public decimal percentage { get; set; } = default!;
+    public double percentage { get; set; }
 
     public string device { get; set; } = default!;
 
