6ff6c0f2ee5a7edfc4b85c4625f11d9885433dd6
karelsgs2 <65086321+karelsgs2@users.noreply.github.com>
2026-02-04 19:41:36 +0100

    Update UserService.cs

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,12 +1,14 @@
+
+using Kosync.Database.Entities;
+
 namespace Kosync.Services;
 
 public class UserService
 {
-    private IHttpContextAccessor _contextAccessor;
-    private KosyncDb _db;
+    private readonly IHttpContextAccessor _contextAccessor;
+    private readonly KosyncDb _db;
 
     private bool userLoadAttempted = false;
-
 
     private string? _username = "";
     public string? Username
@@ -48,7 +50,6 @@ public class UserService
         }
     }
 
-
     public UserService(IHttpContextAccessor contextAccessor, KosyncDb db)
     {
         _contextAccessor = contextAccessor;
@@ -57,25 +58,21 @@ public class UserService
 
     private void LoadUser()
     {
-        if (userLoadAttempted) { return; }
-
+        if (userLoadAttempted) return;
         userLoadAttempted = true;
 
         _username = _contextAccessor?.HttpContext?.Request.Headers["x-auth-user"];
-
         string? passwordHash = _contextAccessor?.HttpContext?.Request.Headers["x-auth-key"];
 
-        var userCollection = _db.Context.GetCollection<User>("users");
+        if (string.IsNullOrEmpty(_username) || string.IsNullOrEmpty(passwordHash)) return;
 
+        var userCollection = _db.Context.GetCollection<User>("users");
         var user = userCollection.FindOne(i => i.Username == _username && i.PasswordHash == passwordHash);
 
-        if (user is null) { return; }
-
+        if (user is null) return;
 
         _isAuthenticated = true;
-
         _isActive = user.IsActive;
-
         _isAdmin = user.IsAdministrator;
     }
 }
