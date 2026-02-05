5fe543fa62a2fd40bebd6fd5b4daee29c53c6c7a
karelsgs2 <65086321+karelsgs2@users.noreply.github.com>
2026-02-05 22:05:18 +0100

    Update GlobalUsings.cs

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,12 +1,8 @@
-
 global using Kosync.Database;
+global using Kosync.Database.Entities;
 global using Kosync.Models;
 global using Kosync.Services;
 global using LiteDB;
-global using Microsoft.Extensions.Logging;
-global using Microsoft.AspNetCore.Http;
 global using System;
-global using System.Collections.Generic;
-global using System.Linq;
 global using System.Security.Cryptography;
 global using System.Text;
