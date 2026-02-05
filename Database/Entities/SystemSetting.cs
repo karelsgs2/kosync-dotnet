0abf17164c781e8209a07d4da1b80fb1abcab72f
karelsgs2 <65086321+karelsgs2@users.noreply.github.com>
2026-02-05 22:09:06 +0100

    Update SystemSetting.cs

--- Database/Entities/SystemSetting.cs
+++ Database/Entities/SystemSetting.cs
@@ -3,7 +3,6 @@ namespace Kosync.Database.Entities;
 
 public class SystemSetting
 {
-    [BsonId]
     public int Id { get; set; }
     public string Key { get; set; } = default!;
     public string Value { get; set; } = default!;
